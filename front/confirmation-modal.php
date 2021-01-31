a3c315bc8793952c41dcca464e31ab457af5a4a3
Matheus Maldonado da Silva <47877623+matheusmaldonadosilva@users.noreply.github.com>
2021-01-31 17:35:20 -0300

    Add files via upload

---
+++ front/confirmation-modal.php
@@ -0,0 +1,21 @@
+<!-- START MODAL CONFIRMATION -->
+<div class="modal fade" id="confirmation-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <img class="img-logo-sing-in" src="../img/logo.png" alt="">
+            <img class="sing-in-image" src="../img/sign-in.jpg" alt="">
+            <div class="modal-header">
+                <h5 class="modal-title modal-title-confirmation" id="exampleModalLabel">Confirma seu email!</h5>
+                <button type="button" class="close close-button-sign-in" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+            </div>
+            <div class="modal-body">
+                <span class="span-confirmation"> Sua conta foi registrada com sucesso. Para concluir o processo,
+                        verifique seu e-mail para uma solicitação de validação.</span>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!--END MODAL CONFIRMATION -->
---
+++ front/header.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Icones -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@600&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400&display=swap" rel="stylesheet">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" href="../img/icon.png" type="image/x-icon" />
+    <title>Desenvouve</title>
+</head>
+
+<body class="desenvouve-body">
+<!--START NAVBAR-->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a href="index.php">
+            <img class="logo-img" src="../img/logo.png">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Linguagens<i class="fas fa-globe icon-language"></i>
+              </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a href="language.php" class="dropdown-item" href="#">Java<i class="fab fa-java icon-language"></i></a>
+                        <a href="language.php" class="dropdown-item" href="#">Java Script<i class="fab fa-js-square icon-language"></i></a>
+                        <a href="language.php" class="dropdown-item" href="#">React<i class="fab fa-react icon-language"></i></a>
+                    </div>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input href="search.php" class="form-control mr-sm-2" type="search" placeholder="Buscar..." aria-label="Search">
+                <button class="verde_escuro_background btn btn-outline-success my-2 my-sm-0 btn-outline-success-search" type="submit"><i class="fas fa-search"></i></button>
+                <button data-toggle="modal" id="modal-login" data-target="#login-modal" class="btn btn-outline-success my-2 my-sm-0 btn-outline-success-account verde_escuro_background" type="button">Acessar<i class="fas fa-user icon-language "></i></button>
+
+            </form>
+        </div>
+        <a href="./profile.php">
+            <div class="card-user">
+                <img class="card-img-top-header" src="../img/perfil.svg" alt="Card image cap">
+            </div>
+        </a>
+    </nav>
+<!--END NAVBAR-->
